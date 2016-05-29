576852f7526f37c10c515cd15f315d7d0da5211f
HolyWaterA <sua12267@naver.com>
2016-05-30 01:26:39 +0900

    change file type

---
+++ datepage.php
@@ -0,0 +1,33 @@
+<!Doctype html>
+<html>
+<head>
+	<meta charset = "utf-8">
+	<title>Date Page</title>
+	<link href='https://fonts.googleapis.com/css?family=Hind' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="static/css/style.css">
+	<link rel="stylesheet" type="text/css" href="static/css/datepage.css">
+	<style type = "text/css">
+	a{text-decoration:none}
+	</style>
+	</head>
+	<body>
+	<br><br>
+	<a href="#"><div class = "datapage_back">< back </div></a><br>
+	<h6></h6>
+	<p style= "text-align: center;">When is your trip?</p>
+	<div class ="datepage_div">
+		<a href="#"><img src="static/image/calendar_grey.png"></a>
+		<input type="text" name = "date"">
+	</div><br>
+	<p style= "text-align: center;">Where will you stay?</p>
+	<div class ="datepage_div">
+		<a href="#"><img src="static/image/accomodation_grey.png"></a>
+		<input type="text" name="place">
+	</div><br>
+	<a href="#"><div class = "datepage_roundclick">let's do this</div></a>
+	</body>
+	<footer>
+	</footer>
+</html>
+
+
