bd8e6bc518e3cac653d5b68593d44b4a04a8d0bf
HoYean <comforest@naver.com>
2016-05-29 18:08:30 +0900

    Dialog 수정 중

---
+++ plan.php
@@ -0,0 +1,25 @@
+<!-- 
+작성자 : 이호연
+페이지 설명 : Plan page
+-->
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	<link href='https://fonts.googleapis.com/css?family=Hind' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="static/css/style.css">
+	<link rel="stylesheet" type="text/css" href="static/css/navbar_style.css">
+	<link rel="stylesheet" type="text/css" href="static/css/dialog.css">
+	<link rel="stylesheet" type="text/css" href="static/css/path.css">
+	
+</head>
+<body>
+
+	<?php
+		require_once("navbar.inc");
+		require_once("path.inc");
+	?>
+
+</body>
+</html>
