f2192b999a24cdd3514cf2ef0c3742e9f88f7130
HolyWaterA <sua12267@naver.com>
2016-05-30 19:46:53 +0900

    correct error in adduser

--- index.php
+++ index.php
@@ -47,21 +47,22 @@ else $loggedin = FALSE;
                     params = "user=" + userID + "&userName=" + userName;
                     
                     request = new ajaxRequest();
-                    request.open("POST", "adduser.php", true);
-                    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-                    request.setRequestHeader("Content-length", params.length);
-                    request.setRequestHeader("Connection", "close");
+                    request.open("GET", "adduser.php?user=", true);
                     console.log('check1');
 
-                    request.onreadystatechange = function() {
+                    $.post( "adduser.php", { user: userID, time: userName })
+                      .done(function( data ) {
+                        alert( "Data Loaded: " + data );
+                    });
+                    /*request.onreadystatechange = function() {
                         if (this.readyState == 4)
                             if (this.status == 200)
                                 if (this.responseText != null)
                                     document.getElementById('status').innerHTML = this.responseText;
                     }
-                    request.send(params);
+                    request.send(params);*/
                     console.log('check2');
-                    location.replace('theme.php');
+                    //location.replace('theme.php');
                     
                 } else if (response.status === 'not_authorized') {
                     // The person is logged into Facebook, but not your app.
