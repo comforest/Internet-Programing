e270e6fd71fb7892c258993b26c696dffa1cfc48
HolyWaterA <sua12267@naver.com>
2016-05-30 14:58:59 +0900

    correct error in database set up file

--- functions.php
+++ functions.php
@@ -20,9 +20,11 @@ function createTable($name, $query) {
 }
 
 function queryMysql($query) {
-    /*$result = */mysqli_query($server, $query) or die("쿼리 실패: $query");
+    if (!$server)
+        die("!!쿼리를 하고 싶으나 서버 연결 안 됨!!")
+    $result = mysqli_query($server, $query) or die("쿼리 실패: $query");
     echo "this is " . __FILE__ . ": " . __FUNCTION__ . "OK.";
-    //return $result;
+    return $result;
 }
 
 function destroySession() {
