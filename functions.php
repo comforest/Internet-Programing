739cba05815ebcd5d58412ee3e8e75d7c95c2c1b
HolyWaterA <sua12267@naver.com>
2016-05-30 14:55:27 +0900

    correct error in database set up file

--- functions.php
+++ functions.php
@@ -14,15 +14,15 @@ mysqli_select_db($server, $dbname) or die("!!DB 선택 에러!!");
 echo "this is " . __FILE__ . ": " . __LINE__;
 
 function createTable($name, $query) {
-    queryMysql("CREATE TABLE IF NOT EXISTS $name($query);") or die("테이블 생성 실패" . mysqli_error($server));
+    queryMysql("CREATE TABLE IF NOT EXISTS $name($query)") or die("테이블 생성 실패" . mysqli_error($server));
     echo "Table '$name' created or already exists.<br />";
     echo "this is " . __FILE__ . ": " . __FUNCTION__ . "OK.";
 }
 
 function queryMysql($query) {
-    $result = mysqli_query($server, $query) or die("쿼리 실패: $query");
+    /*$result = */mysqli_query($server, $query) or die("쿼리 실패: $query");
     echo "this is " . __FILE__ . ": " . __FUNCTION__ . "OK.";
-	 return $result;
+    //return $result;
 }
 
 function destroySession() {
