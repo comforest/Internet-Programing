7d4d48e8d01b356d00088c6a7817285c422d44dc
HolyWaterA <sua12267@naver.com>
2016-05-31 15:24:29 +0900

    correct error in post request for adduser php

--- adduser.php
+++ adduser.php
@@ -5,15 +5,12 @@ if (isset($_POST['userID']))
 {
     $userID = sanitizeString($_POST['userID']);
     $userName = sanitizeString($_POST['userName']);
-    /*
-    if (mysqli_num_rows(queryMysql($connect, "SELECT * FROM members
-        WHERE user='$userID'")))
-        echo "console.log(\"유저가 이미 있음.\");";
+    queryMysql($connect, "SELECT * FROM members WHERE user='$userID'");
+    /*if (mysqli_num_rows(queryMysql($connect, "SELECT * FROM members WHERE user='$userID'")))
+        null;
     else {
-        echo "console.log(__FILE__ . __LINE__ . \"유저 생성 시도.\");";
         queryMysql($connect, "INSERT INTO user VALUES($userID, '$userName')");
         
     }*/
-    echo "console.log(__FILE__ . __LINE__ . \"adduser.php의 끝부분 도착.\");";
 }
 ?>
