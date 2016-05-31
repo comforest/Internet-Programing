6946b56a8f74ad4a5bbd727a0ceafc8b2f063f50
HolyWaterA <sua12267@naver.com>
2016-05-31 15:10:05 +0900

    correct error in post request for adduser php

--- adduser.php
+++ adduser.php
@@ -7,10 +7,10 @@ if (isset($_POST['userID']))
     $userName = sanitizeString($_POST['userName']);
     
     echo "this is " . __FILE__ . ": " . __FUNCTION__;
-    /*if(!$connect){
+    if(!$connect){
         echo("__FILE__ . ": " . __FUNCTION__" . "mysql 연결 안 됨");
     }
-    if (mysqli_num_rows(queryMysql($connect, "SELECT * FROM members
+    /*if (mysqli_num_rows(queryMysql($connect, "SELECT * FROM members
         WHERE user='$userID'")))
         echo("<script>location.replace('theme.php');</script>");
     else
