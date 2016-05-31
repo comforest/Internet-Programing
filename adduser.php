df44d50875500ce3bdc93c54ec366291f1503332
HolyWaterA <sua12267@naver.com>
2016-05-31 14:21:22 +0900

    correct error in post request for adduser php

--- adduser.php
+++ adduser.php
@@ -6,6 +6,7 @@ if (isset($_POST['userID']))
     $userID = sanitizeString($_POST['userID']);
     $userName = sanitizeString($_POST['userName']);
 
+    echo "this is " . __FILE__ . ": " . __FUNCTION__;
     if(!$connect)
         die("__FILE__ . ": " . __FUNCTION__" . "mysql 연결 안 됨");
     if (mysqli_num_rows(queryMysql($connect, "SELECT * FROM members
--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@ $dbuser  = 'traversapp';
 $dbpass  = 'haveagoodtrip';
 $appname = "Travers";
 
-$connect = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
+global $connect = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
 // Check connection
 if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
