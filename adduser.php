9ce4fc9897834c12ea101d980dd5fe46f7c364d3
HolyWaterA <sua12267@naver.com>
2016-05-31 22:13:37 +0900

    correction

--- adduser.php
+++ adduser.php
@@ -7,6 +7,7 @@ if (isset($_POST["userinfo"]))
     $userName = $_POST['userinfo2'];
     echo "check1!";
     $numOfuser = queryMysql($connect, "SELECT * FROM user WHERE userID='$userID'");
+    echo "유저 수" . $numOfuser;
     if (mysqli_num_rows($numOFuser) == 0) {
         queryMysql($connect, "INSERT INTO user VALUES('$userID', '$userName')");
         echo "check2";
