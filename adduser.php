301444ac27d42e7d6776f5209860c0b6e4ec9704
HolyWaterA <sua12267@naver.com>
2016-05-31 21:07:23 +0900

    change way to post

--- adduser.php
+++ adduser.php
@@ -6,12 +6,18 @@ if (isset($_POST['userinfo']))
     $userID = $_POST['userinfo'];
     $userName = $_POST['userinfo2'];
     echo "userID: " . $userID . "userName: " . $userName;
+    echo __FILE__ . ": " . __LINE__;
     $numOfuser = mysqli_query($connect, "SELECT * FROM user WHERE userID='$userID'", MYSQLI_USE_RESULT) or die("쿼리 실패: $query". mysqli_error($connect));
+    echo __FILE__ . ": " . __LINE__;
     // $numOfuser = queryMysql($connect, "SELECT * FROM user WHERE userID='$userID'");
     echo "유저 수: " . $numOfuser;
+    echo __FILE__ . ": " . __LINE__;
     if (mysqli_num_rows($numOfuser) == 0) {
         //queryMysql($connect, "INSERT INTO `user` VALUES('$userID', '$numOfuser')");
         mysqli_query($connect, "INSERT INTO `user` VALUES('$userID', '$numOfuser')", MYSQLI_USE_RESULT) or die("쿼리 실패: $query". mysqli_error($connect));
+        echo __FILE__ . ": " . __LINE__;
     }
+    echo __FILE__ . ": " . __LINE__;
+    echo "<script>location.replace('theme.php');</script>"
 }
 ?>
