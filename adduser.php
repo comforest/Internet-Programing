36056de8e70c80a9ff51b5837bf999a4b5175841
HolyWaterA <sua12267@naver.com>
2016-05-31 16:56:24 +0900

    correct user info collision

--- adduser.php
+++ adduser.php
@@ -6,9 +6,7 @@ if (isset($_POST['userID']))
     $userID = $_POST['userID'];
     $userName = $_POST['userName'];
     
-    if (mysqli_num_rows(queryMysql($connect, "SELECT * FROM user WHERE userID='$userID'")))
-        null;
-    else {
+    if (!mysqli_num_rows(queryMysql($connect, "SELECT * FROM user WHERE userID='$userID'"))) {
         queryMysql($connect, "INSERT INTO user VALUES('$userID', '$userName')");
     }
 }
