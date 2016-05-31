3b659d1b78a8c6b2cda67a99f6cdd6ff9de1c5bb
HolyWaterA <sua12267@naver.com>
2016-05-31 13:55:50 +0900

    correct error in post request for adduser php

--- adduser.php
+++ adduser.php
@@ -1,7 +1,7 @@
 <?php // Example 21-6: checkuser.php
 require_once 'functions.php';
 
-if (isset($_POST['user']))
+if (isset($_POST['userID']))
 {
     $userID = sanitizeString($_POST['userID']);
     $userName = sanitizeString($_POST['userName']);
