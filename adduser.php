c0854e1cc221c6bc3d7cfe7ffe8f2007ac206e29
HolyWaterA <sua12267@naver.com>
2016-05-31 15:53:30 +0900

    correct error in post request for adduser php

--- adduser.php
+++ adduser.php
@@ -3,8 +3,8 @@ require_once 'functions.php';
 
 if (isset($_POST['userID']))
 {
-    $userID = (int) sanitizeString($_POST['userID']);
-    $userName = sanitizeString($_POST['userName']);
+    $userID = (int) $_POST['userID'];
+    $userName = $_POST['userName'];
     
     $result = queryMysql($connect, "INSERT INTO user VALUES($userID, '$userName')");
     /*if (!$result)
