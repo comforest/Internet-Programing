43569964e3c7c62df7a40b53f91e9418f7b152a9
HolyWaterA <sua12267@naver.com>
2016-05-31 15:33:15 +0900

    correct error in post request for adduser php

--- adduser.php
+++ adduser.php
@@ -5,7 +5,7 @@ if (isset($_POST['userID']))
 {
     $userID = (int) sanitizeString($_POST['userID']);
     $userName = sanitizeString($_POST['userName']);
-    
+    queryMysql($connect, "INSERT INTO user VALUES($userID, '$userName')");
     if (mysqli_num_rows($connect, queryMysql($connect, "SELECT * FROM members WHERE user='$userID'")))
         null;
     else {
