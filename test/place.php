f567013d40912ecf66e11a3eed504cf7d9083b62
waylight3 <waylight3@naver.com>
2016-06-01 20:22:21 +0900

    go seoul

--- test/place.php
+++ test/place.php
@@ -37,7 +37,7 @@
 			var infowindow;
 
 			function initMap() {
-			  var pyrmont = {lat: -33.867, lng: 151.195};
+			  var pyrmont = {lat: 37.55, lng: 127.0};
 
 			  map = new google.maps.Map(document.getElementById('map'), {
 			    center: pyrmont,
