487c059310db00b325e0b0ef093d225c001b98ac
HoYean <comforest@naver.com>
2016-06-02 00:58:50 +0900

    search/detail mobile test

--- search.php
+++ search.php
@@ -3,7 +3,7 @@
 페이지 설명 : Serach page
 -->
 <?php // theme.php
-require_once 'header.php';
+// require_once 'header.php';
 // if (!isset($_POST['date']) || !isset($_POST['place']))
 //     echo("<script>location.replace('datepage.php');</script>");
 // $_SESSION['date'] = $_POST['date'];
