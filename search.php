ccc69fbc86cced38d01a4356e2bfb017e0fbc469
waylight3 <waylight3@naver.com>
2016-06-02 19:40:23 +0900

    plz..;;

--- search.php
+++ search.php
@@ -35,7 +35,6 @@
 		    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 </head>
 <body>
-
 	<?php
 		require_once("navbar.inc");
 		require_once("datebar.inc");
