13a6410726b57584825a0214216aec3a91d626ac
waylight3 <waylight3@naver.com>
2016-06-02 21:04:34 +0900

    fix progress bar on theme

--- theme.php
+++ theme.php
@@ -40,11 +40,11 @@
             <input type="hidden" name="themeinfo" id="themeinfo" value="">
     </form>
     <div class="progress-box">
-    	<img src="/static/image/culture_grey.png" style="width: 5%;">
-    	<img src="/static/image/progresslinegreylong.png" style="width: 10%; margin-bottom: 1.5%;">
-    	<img src="/static/image/culture_grey.png" style="width: 5%;">
-    	<img src="/static/image/progresslinegreylong.png" style="width: 10%; margin-bottom: 1.5%;">
-    	<img src="/static/image/culture_grey.png" style="width: 5%;">
+    	<img src="/static/image/culture_grey.png" style="width: 4%;">
+    	<img src="/static/image/progresslinegreylong.png" style="width: 10%; margin-bottom: 1.2%;">
+    	<img src="/static/image/culture_grey.png" style="width: 4%;">
+    	<img src="/static/image/progresslinegreylong.png" style="width: 10%; margin-bottom: 1.2%;">
+    	<img src="/static/image/culture_grey.png" style="width: 4%;">
     </div>
 	<div class="themeupper">Set the theme for your travel</div>
 	<div class="themebox">
