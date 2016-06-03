4f608f9d115a8b250472fef440f80ef44b6ee3e2
waylight3 <waylight3@naver.com>
2016-06-03 19:05:26 +0900

    change url settings

---
+++ attraction/index.php
@@ -0,0 +1,80 @@
+<!-- 
+작성자 : 이호연
+페이지 설명 : Serach page
+-->
+<?php // theme.php
+// require_once 'header.php';
+// if (!isset($_POST['date']) || !isset($_POST['place']))
+//     echo("<script>location.replace('datepage.php');</script>");
+// $_SESSION['date'] = $_POST['date'];
+// $_SESSION['place'] = $_POST['place'];
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	<link href='https://fonts.googleapis.com/css?family=Hind' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="static/css/style.css">
+	<link rel="stylesheet" type="text/css" href="static/css/navbar_style.css">
+	<link rel="stylesheet" type="text/css" href="static/css/dialog.css">
+	<link rel="stylesheet" type="text/css" href="static/css/tourlist.css">
+	<link rel="stylesheet" type="text/css" href="static/css/datebar.css">
+	<link rel="stylesheet" type="text/css" href="static/css/tab.css">
+	<meta name="viewport" content="initial-scale=1.0">
+    		<meta charset="utf-8">
+		    <style>
+		    	html, body {
+		    		height: 100%;
+		    	}
+		    	#mapbox, #map {
+		    		height: 100%;
+		    		width: 100%;
+		    		z-index: -10;
+		    		position: absolute;
+		    	}
+		    </style>
+		    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+</head>
+<body>
+	<?php
+		require_once("navbar.inc");
+		require_once("datebar.inc");
+		require_once("tourlist.inc");
+		require_once("tab.inc");
+	?>
+	<div id="mapbox">
+		<div id="map"></div>
+	</div>
+	<script>
+		function initMap() {
+			var chicago = {lat: 41.85, lng: -87.65};
+			var indianapolis = {lat: 39.79, lng: -86.14};
+
+			var map = new google.maps.Map(document.getElementById('map'), {
+				center: chicago,
+				zoom: 7
+			});
+
+			var directionsDisplay = new google.maps.DirectionsRenderer({
+				map: map
+			});
+
+			var request = {
+				destination: indianapolis,
+				origin: chicago,
+				travelMode: google.maps.TravelMode.DRIVING
+			};
+
+			var directionsService = new google.maps.DirectionsService();
+			directionsService.route(request, function(response, status) {
+				if (status == google.maps.DirectionsStatus.OK) {
+					directionsDisplay.setDirections(response);
+				}
+			});
+		}
+	</script>
+
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCG21Y5X-wARtfSC6WkgO1nxoVU0WwcjwE&callback=initMap" async defer></script>
+</body>
+</html>
---
+++ set/theme/index.php
@@ -0,0 +1,93 @@
+<?php // theme.php
+   // require_once 'header.php';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Theme Page</title>
+	<link href='https://fonts.googleapis.com/css?family=Hind' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="static/css/style.css">
+	<link rel="stylesheet" type="text/css" href="static/css/theme_style2.css">
+	<link rel="stylesheet" type="text/css" href="static/css/progress.css">
+    <script>
+        function cilck_culture_theme() {
+            document.getElementById("themeinfo").setAttribute('value', 'culture');
+            document.getElementById("themeinfoForm").submit();
+        }
+        function cilck_shopping_theme() {
+            document.getElementById("themeinfo").setAttribute('value', 'shopping');
+            document.getElementById("themeinfoForm").submit();
+        }
+        function cilck_nature_theme() {
+            document.getElementById("themeinfo").setAttribute('value', 'nature');
+            document.getElementById("themeinfoForm").submit();
+        }
+        function cilck_trend_theme() {
+            document.getElementById("themeinfo").setAttribute('value', 'trend');
+            document.getElementById("themeinfoForm").submit();
+        }
+        function cilck_nothing_theme() {
+            document.getElementById("themeinfo").setAttribute('value', 'nothing');
+            document.getElementById("themeinfoForm").submit();
+        }
+    </script>
+</head>
+<body style="background-color: #f3f3f3;">
+	<?php
+		require_once 'navbar.inc';
+	?>
+    <form action="datepage.php" method="post" id="themeinfoForm">
+            <input type="hidden" name="themeinfo" id="themeinfo" value="">
+    </form>
+    
+    <div class = "progress-box">
+    	<img src = "static/image/theme_pro.png">
+    </div>
+
+
+	<div class="themebox">
+		<div class="themeinner" onclick="cilck_culture_theme()">
+            <div class="innerimage"><img src="static/image/culture_theme.png"/></div>
+			<div class="innercontent">
+				<div> Culture </div>
+				<p>
+					Experience interesting local culture, history and beauty
+				</p>
+			</div>
+		</div>
+		<div class="themeinner" onclick="cilck_shopping_theme()">
+			<div class="innerimage"><img id = "shopping_theme" src="static/image/shopping_theme.png"/></div>
+			<div class="innercontent">
+				<div> Shopping </div>
+				<p>
+					Spend money and shop till you drop with the best deals
+				</p>
+			</div>
+		</div>
+		<div class="themeinner" onclick="cilck_nature_theme()">
+			<div class="innerimage"><img id = "nature_theme" src="static/image/nature_theme.png"/></div>
+			<div class="innercontent">	
+				<div> Nature </div>
+				<p>
+					Spend some quality time with the best of mother nature
+				</p>
+			</div>
+		</div>
+		<div class="themeinner" onclick="cilck_trend_theme()">
+			<div class="innerimage"><img id = "trend_theme" src="static/image/trend_theme.png"/></div>
+			<div class="innercontent">
+				<div> Trend </div>
+				<p>
+					Get wild and crazy by experiencing hot local trends
+				</p>
+			</div>
+		</div>
+	</div>
+	<div class="themelower">
+		<div> okay, cool </div>
+		<!--div onclick="cilck_nothing_theme()"> nah, nevermind</div-->
+	</div>
+	<p onclick="cilck_nothing_theme()" id = "nevermind">nah, nevermind</p>
+</body>
+</html>
