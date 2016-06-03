87d56c662b0b1d1c8677259d011e79b317553e1f
waylight3 <waylight3@naver.com>
2016-06-03 21:56:31 +0900

    fix index

--- include/header.php
+++ include/header.php
@@ -4,12 +4,11 @@
             세션에서 유저 정보등을 받아와 변수들에 저장.
             2. 로그인 상태가 아니면 로그인 페이지로 돌아간다.
 -->
-
-<?php // header.php
-session_start();
-require_once 'functions.php';
-//$_SESSION['userID'] = '1065100166913049';
-if (!isset($_SESSION['userID'])) {
-    echo("<script>location.replace('index.php');</script>");
-}
+<?php
+	session_start();
+	require_once($_SERVER['DOCUMENT_ROOT'].'/include/functions.php');
+	//$_SESSION['userID'] = '1065100166913049';
+	if (!isset($_SESSION['userID'])) {
+	    echo("<script>location.replace('/');</script>");
+	}
 ?>
