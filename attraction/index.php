f7258d194d47c552a02020f724f1ccf2f2af3fb6
waylight3 <waylight3@naver.com>
2016-06-03 19:36:18 +0900

    fixing attraction

--- attraction/index.php
+++ attraction/index.php
@@ -11,12 +11,12 @@
 	<meta charset="utf-8">
 	<title></title>
 	<link href='https://fonts.googleapis.com/css?family=Hind' rel='stylesheet' type='text/css'>
-	<link rel="stylesheet" type="text/css" href="static/css/style.css">
-	<link rel="stylesheet" type="text/css" href="static/css/navbar_style.css">
-	<link rel="stylesheet" type="text/css" href="static/css/dialog.css">
-	<link rel="stylesheet" type="text/css" href="static/css/tourlist.css">
-	<link rel="stylesheet" type="text/css" href="static/css/datebar.css">
-	<link rel="stylesheet" type="text/css" href="static/css/tab.css">
+	<link rel="stylesheet" type="text/css" href="/static/css/style.css">
+	<link rel="stylesheet" type="text/css" href="/static/css/navbar_style.css">
+	<link rel="stylesheet" type="text/css" href="/static/css/dialog.css">
+	<link rel="stylesheet" type="text/css" href="/static/css/tourlist.css">
+	<link rel="stylesheet" type="text/css" href="/static/css/datebar.css">
+	<link rel="stylesheet" type="text/css" href="/static/css/tab.css">
 	<meta name="viewport" content="initial-scale=1.0">
     		<meta charset="utf-8">
 		    <style>
