f1a4bf310625a190bb60a5d4f5ba0cc1c860aaae
waylight3 <waylight3@naver.com>
2016-06-04 18:39:24 +0900

    add image marker to the map on the attraction page

--- attraction/index.php
+++ attraction/index.php
@@ -56,8 +56,8 @@
                             map:map,
                             icon: {
                                 url: '/static/image/round1.png',
-                                size: new google.maps.Size(20, 32);
-                                origin: new google.maps.Origin(0, 0);
+                                size: new google.maps.Size(20, 32),
+                                origin: new google.maps.Origin(0, 0),
                                 anchor: new google.maps.Point(0, 32)
                             }
                         });
