3f405d99da1e02a68d4f2d8a8776b150156b6316
HolyWaterA <sua12267@naver.com>
2016-06-04 17:04:13 +0900

    fix test

--- include/dateTest.php
+++ include/dateTest.php
@@ -1,6 +1,8 @@
 <?php // dateTest.php
 
-if (!isset($_POST['dateinfo']))
+if (!(isset($_POST['dateStartinfo']) || isset($_SESSION['dateStart'])))
+    echo("<script>location.replace('/set/theme');</script>");
+if (!(isset($_POST['dateEndinfo']) || isset($_SESSION['dateEnd'])))
     echo("<script>location.replace('/set/theme');</script>");
 $_SESSION['dateStart'] = $_POST['dateStartinfo'];
 $_SESSION['dateEnd'] = $_POST['dateEndinfo'];
