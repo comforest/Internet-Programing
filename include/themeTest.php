51d513ca41d289adeb3f145f7c988274d1c9f1e1
HolyWaterA <sua12267@naver.com>
2016-06-04 17:30:17 +0900

    fix

--- include/themeTest.php
+++ include/themeTest.php
@@ -2,6 +2,7 @@
 
 if (!(isset($_POST['themeinfo']) || isset($_SESSION['theme'])))
     echo("<script>location.replace('/set/theme');</script>");
-$_SESSION['theme'] = $_POST['themeinfo'];
+if (isset($_POST['themeinfo'])
+    $_SESSION['theme'] = $_POST['themeinfo'];
 
 ?>
