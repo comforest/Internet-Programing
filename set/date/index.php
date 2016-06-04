38df24d93951316cf90baa3ebd921b89e16da675
HolyWaterA <sua12267@naver.com>
2016-06-04 11:13:42 +0900

    correct link

--- set/date/index.php
+++ set/date/index.php
@@ -57,7 +57,7 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/include/themeTest.php');
 
 
 
-        <form action="/set/hotel/" method="post" id="datePlaceInfoForm">
+        <form action="/set/hotel/" method="post" id="dateInfoForm">
             <div class = "datepage_space1">&nbsp; </div>
             <div class = "datepage_word" id = "dateStartinfo">from when?</div>
             <div class="datepage_space2">&nbsp;</div>
