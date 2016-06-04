7871f723c248541fd902a14874132bacb449e3e6
HolyWaterA <sua12267@naver.com>
2016-06-04 15:17:45 +0900

    resize marker

--- set/hotel/index.php
+++ set/hotel/index.php
@@ -183,7 +183,11 @@ function search() {
       var image = {
         url: MARKER_PATH,
         // This marker is 20 pixels wide by 32 pixels high.
-        size: new google.maps.Size(35, 35)
+        size: new google.maps.Size(35, 35),
+        // The origin for this image is (0, 0).
+        origin: new google.maps.Point(0, 0),
+        // The anchor for this image is the base of the flagpole at (0, 32).
+        anchor: new google.maps.Point(0, 32)
       };
       // Create a marker for each hotel found, and
       // assign a letter of the alphabetic to each marker icon.
