80de24450c4d94babb41ab234f573214b46ed3b7
waylight3 <waylight3@naver.com>
2016-06-06 23:27:00 +0900

    add icon on the map

--- attraction/index.php
+++ attraction/index.php
@@ -56,7 +56,8 @@
                     size: new google.maps.Size(400, 400),
                     origin: new google.maps.Point(0, 0),
                     anchor: new google.maps.Point(0, 0),
-                    scaledSize: new google.maps.Size(50, 50)
+                    scaledSize: new google.maps.Size(50, 50),
+                    path: google.maps.SymbolPath.CIRCLE
                 };
                 if (place.photos) {
                     image.url = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=' + place.photos[0].photo_reference + '&key=AIzaSyAYOKpuX6_Y9muKZCB4rBX7xiBCJKAJ2RQ'
