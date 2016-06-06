cc9eaf72cf3e8d01fbae08c9fdf9137de9a8415e
waylight3 <waylight3@naver.com>
2016-06-06 17:24:01 +0900

    test ajax

--- attraction/index.php
+++ attraction/index.php
@@ -67,6 +67,8 @@
                         url:"https://maps.googleapis.com/maps/api/place/details/json?placeid=" + place.place_id + "&key=AIzaSyCG21Y5X-wARtfSC6WkgO1nxoVU0WwcjwE",
                         dataType:"jsonp",
                         contentType:"application/json",
+                        async:false,
+                        jsonCallback:"jsonCallback",
                         success: function(data) {
                             console.log(data);
                         },
