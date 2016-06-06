da861fd30e9454ed3f70831d7d926085688cf0f9
HolyWaterA <sua12267@naver.com>
2016-06-06 19:03:55 +0900

    save set information in mysql

--- include/hotelTest.php
+++ include/hotelTest.php
@@ -4,11 +4,14 @@
     if (!(isset($_POST['hotelinfo']) || isset($_SESSION['hotel'])))
         echo("<script>location.replace('/set/theme');</script>");
 
+    echo("\n\n 호텔 테스트 시작!");
     if (isset($_POST['cityinfo']) && isset($_POST['hotelinfo'])) {
         $_SESSION['hotelCity'] = $_POST['cityinfo'];
         $_SESSION['hotel'] = $_POST['hotelinfo'];
         $_SESSION['hotelID'] = $_POST['hotelIDinfo'];
         
+        echo("호텔 저장 쿼리 전");
+        echo($_SESSION['hotelID']);
         //처음 저장하는 유저면 추가, 원래 있던 유저면 정보 업데이트 :
         queryMysql($connect, "UPDATE plan set hotelID= '" . $_SESSION['hotelID'] . "' WHERE planID= '" . $_SESSION['planID'] .  "'");
         echo("호텔 정보도 저장!");
