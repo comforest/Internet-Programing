4710707d6dd77a88e809b7fb0de4e82b94b3767e
HolyWaterA <sua12267@naver.com>
2016-06-06 22:51:46 +0900

    correct info window error in hotelpage

--- set/hotel/index.php
+++ set/hotel/index.php
@@ -178,8 +178,6 @@ function search() {
         url: '/static/image/marker_hotel.png',
         // This marker is 20 pixels wide by 32 pixels high.
         size: new google.maps.Size(500, 500),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(0, 0),
         scaledSize: new google.maps.Size(60, 60)
       };
       // Create a marker for each hotel found, and
