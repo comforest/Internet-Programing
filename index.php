ff244b282320df58e9021f637b8a6736dbf7b0a6
faizwong <faiznazri0907@gmail.com>
2016-06-06 15:44:21 +0900

    fix index page

--- index.php
+++ index.php
@@ -25,20 +25,10 @@
             <div class = "container text-center">
                 <nav class = "nav navbar-default">
                     <div class = "container-fluid">
-                        <div class = "navbar-header">
-                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse" aria-expanded="false">
-                            <span class="sr-only">Toggle navigation</span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            </button>
                             <a class = "navbar-brand" href = "#"><img src = "static/image/logo_white.png" id = "logo_img" class = "img-responsive"/></a>
-                        </div>
-                        <div class="collapse navbar-collapse" id=".navbar-collapse">
-                        <ul class = "nav navbar-nav navbar-right">
-                            <li><a href = "about.html"><p>About</p></a></li>
+                        <ul class = "nav navbar-nav navbar-right pull-right about">
+                            <li><a href = "/about/index.html"><p>About</p></a></li>
                         </ul>
-                        </div>
                     </div>
                 </nav>
                 <div class = "main-text">
