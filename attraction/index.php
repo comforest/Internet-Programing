49fac62322d3f0ff875190d47a63cc50ca619d55
waylight3 <waylight3@naver.com>
2016-06-06 17:06:24 +0900

    fixing map on the attraction

--- attraction/index.php
+++ attraction/index.php
@@ -61,7 +61,7 @@
                 });
                 
                 google.maps.event.addListener(marker, 'click', function() {
-                    console.log("이름: " + place.name + ", 위치: " + place.geometry.location + ", 코드: " + place.place_id);
+                    console.log("이름: " + place.name + ", 위치: {" + place.geometry.location.lat + "," + place.geometry.location.lng + "}, 코드: " + place.place_id);
                 })
             }
             
