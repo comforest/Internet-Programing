291f1fd59d913ad47ca7a2a191440841eac6f170
waylight3 <waylight3@naver.com>
2016-06-06 23:53:00 +0900

    add image on detail view

--- attraction/index.php
+++ attraction/index.php
@@ -99,7 +99,7 @@
                         
                         pre_html = $("#dialog_body").html();
                         pre_html += "<article class=\"dialog_article\" data-place-index=" + index_count + ">";
-			            pre_html += "<img src=\"/static/image/sampleImage.jpg\">";
+			            pre_html += "<img src=\"https://maps.googleapis.com/maps/api/place/photo?maxwidth=800&photoreference=" + place.photos[0].photo_reference + "&key=AIzaSyAYOKpuX6_Y9muKZCB4rBX7xiBCJKAJ2RQ\">";
 			            pre_html += "<h1>" + data.name + "</h1>";
 			            pre_html += "<p>" + data.formatted_address + "</p>";
                         pre_html += "<div style=\"clear: both;\"></div>";
@@ -109,7 +109,6 @@
                         place_list.push(data);
                         index_count += 1;
                     });
-                    
                 });
             }
         </script>
